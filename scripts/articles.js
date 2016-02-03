f450dd523ee6afd54567460bf4b9e849755d7e2c
Ruben Sarkisyan <rubens24@uw.edu>
2016-02-02 19:38:45 -0800

    completed class 04 portfolio assignment

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,42 +1,42 @@
 var articles = [];
 
 function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
   this.title = opts.title;
   this.category = opts.category;
-  this.author = opts.author;
-  this.publishedOn = opts.publishedOn;
   this.body = opts.body;
-};
+  this.publishedOn = opts.publishedOn;
+}
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.data('title', this.title);
-  $newArticle.data('category', this.category);
-  $newArticle.data('author', this.author);
-  $newArticle.data('publishedOn', this.publishedOn);
-  $newArticle.data('body', this.body);
+  var template = Handlebars.compile($('#articleHB').text());
 
-  $newArticle.find('h1').html(this.title);
-  $newArticle.find('a').html(this.author);
+  // DONE: If your template will use properties that aren't on the object yet, add them.
+  //   Since your template can't hold any JS logic, we need to execute the logic here.
+  //   The result is added to the object as a new property, which can then be referenced by key in the template.
+  //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  $newArticle.find('section.article-body').html(this.body);
-  $newArticle.find('time[pubdat]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  $newArticle.append('<hr>');
-
-  $newArticle.removeClass('template');
-  return $newArticle;
+   return template(this);
 };
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
+if (typeof rawData !== 'undefined') {
+
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-});
+})
+
+rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+   });
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml());
+  $('#articles').append(a.toHtml())
 });
+}
